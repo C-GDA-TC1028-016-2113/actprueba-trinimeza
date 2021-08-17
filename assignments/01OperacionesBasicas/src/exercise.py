af6c726f30d3f8e55c95dc2ace6b8907a37dd36c
trinimeza <a01634249@itesm.mx>
2021-08-17 17:14:26 +0000

    Actividad enviada

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
+    num1 = int(input("Dame un número: "))
+    num2 = int(input("Dame un número: "))
+    Suma = (num1 + num2)
+    Resta = (num1 - num2)
+    Multiplicación = (num1 * num2)
+    print("Suma:", Suma)
+    print("Resta:", Resta)
+    print("Multiplicación:", Multiplicación)
+
 
 
 
